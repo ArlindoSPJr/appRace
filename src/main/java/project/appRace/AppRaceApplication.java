96b7f43d8f108002b18df835f7d14f61d9df921e
Arlindo Junior <arlindo.junior@sga.pucminas.br>
2025-09-14 18:42:36 -0300

    inicializando o projeto

---
+++ src/main/java/project/appRace/AppRaceApplication.java
@@ -0,0 +1,13 @@
+package project.appRace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppRaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/appRace/AppRaceApplicationTests.java
@@ -0,0 +1,13 @@
+package project.appRace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
